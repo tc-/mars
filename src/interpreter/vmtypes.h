91c86bedeff4c15f10bb19b78847a6606567fde7
Tommy Carlsson <tommy.l.s.carlsson@gmail.com>
2008-06-14 15:15:06 +0200

    Added vmtypes.h

---
+++ src/interpreter/vmtypes.h
@@ -0,0 +1,11 @@
+#ifndef VMTYPES_H
+#define VMTYPES_H
+
+typedef int vmInt;
+typedef unsigned int vmUInt;
+typedef float vmFloat;
+typedef unsigned char vmByte;
+typedef signed char vmSByte;
+typedef unsigned char vmBool;
+
+#endif // VMTYPES_H
